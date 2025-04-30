5198123dfbd1bf650bf20303979af83d7cc46f51
Simon Pirko <sipi7gm@gmail.com>
2025-04-30 21:08:48 +0300

    Delete row

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ public class Main {
     public static void main(String[] args) {
         long number = 1230467089;
 
-        System.out.print("reversed number is: ");
-
         do {
             System.out.print(number % 10);
             number /= 10;
