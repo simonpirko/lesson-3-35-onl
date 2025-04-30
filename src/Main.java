519f161e7f1d47062cea525ef35d00fb10d6104b
Simon Pirko <sipi7gm@gmail.com>
2025-04-30 20:19:11 +0300

    Refactoring

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        long number1 = 1230467089;
+        long number = 1230467089;
 
         System.out.print("reversed number is: ");
 
         do {
-            System.out.print(number1 % 10);
-            number1 /= 10;
-        } while (number1 / 10 != 0);
+            System.out.print(number % 10);
+            number /= 10;
+        } while (number / 10 != 0);
 
-        System.out.println(number1);
+        System.out.println(number);
     }
 }
